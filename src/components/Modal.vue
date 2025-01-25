33b5ef7ea90a043fb8ef957658bb3e139cd16cc4
Itamar Marom <itamarnine4@gmail.com>
2025-01-25 16:41:56 +0200

    Implmenet Modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+
+const emit = defineEmits<{
+    (e: 'close'): void,
+}>()
+</script>
+
+<template>
+    <div class="modal">
+        <div class="container">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.modal {
+    position: fixed;
+    z-index: 1;
+    padding-top: 100px;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgb(0, 0, 0);
+    background-color: rgba(0, 0, 0, 0.4);
+}
+
+.container {
+    background-color: #fefefe;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+    max-width: 600px;
+    height: 400px;
+    border-radius: 6px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+</style>
