dda026998a77df5292d14e18cbda228568a90c02
Itamar Marom <itamarnine4@gmail.com>
2025-01-25 17:42:12 +0200

    Update beta config

--- vite.beta.config.ts
+++ vite.beta.config.ts
@@ -1,11 +1,52 @@
-import { defineConfig } from 'vite';
-import viteConfig from './vite.config.ts';
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import mkcert from 'vite-plugin-mkcert'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  ...viteConfig,
+  server: { https: true },
+  base: '/notepadjs/beta',
   define: {
     IS_BETA: 1,
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
   },
-  base: '/notepadjs/beta'
-});
+  plugins: [
+    VitePWA({
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Notepad - Beta',
+        short_name: 'Notepad - Beta',
+        description: 'A Notepad PWA',
+        theme_color: '#cdddec',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+    vue(),
+    vueJsx(),
+    mkcert()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
